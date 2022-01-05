a83697696c1e6625e775f8fe9483669d69617517
janvalic <valicjan@gmail.com>
2022-01-05 13:17:12 +0100

    App style change

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Kolesarjanci',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Pallete.kToDark,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -74,11 +51,17 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blue.shade900, Colors.green.shade400])),
+    child: Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.black45,
+        title: Text("Kolesarjanci"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -109,8 +92,12 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             ElevatedButton(
+                style: ElevatedButton.styleFrom(
+
+                  primary: Colors.teal,
+                ),
                 onPressed: readJson,
-                child: const Text("Poti")
+                child: const Text("Ogled vseh kolesarskih poti")
             ),
             _items.isNotEmpty
             ? Expanded(
@@ -133,7 +120,8 @@ class _MyHomePageState extends State<MyHomePage> {
             :Container()
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -153,8 +141,16 @@ class FirstRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blue.shade900, Colors.green.shade400])),
+    child: Scaffold(
+    backgroundColor: Colors.transparent,
       appBar: AppBar(
+        backgroundColor: Colors.black45,
         title: Text(naziv),
       ),
       body: Center(
@@ -169,7 +165,7 @@ class FirstRoute extends StatelessWidget {
             ),
             Container(
               alignment: Alignment.topRight,
-              child: Text("Ratio: " + ((like/(like+dislike)*100).toInt()).toString(), style: TextStyle(fontSize: 20)),
+              child: Text("Ocena: " + ((like/(like+dislike)*100).toInt()).toString(), style: TextStyle(fontSize: 20)),
               margin: EdgeInsets.only(right: 20),
             ),
             Container(
@@ -200,5 +196,6 @@ class FirstRoute extends StatelessWidget {
           ],
         ),
       ),
+    ),
     );
   }}
